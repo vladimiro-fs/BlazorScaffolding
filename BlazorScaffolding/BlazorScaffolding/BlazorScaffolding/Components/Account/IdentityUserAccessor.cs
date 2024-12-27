d6955f7f439464ad3a7b03ec44437a73b7c9a2e2
vladimiro-fs <vladzyo.gaming@pm.me>
2024-12-27 15:23:04 +0000

    Init project

---
+++ BlazorScaffolding/BlazorScaffolding/BlazorScaffolding/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+using BlazorScaffolding.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorScaffolding.Components.Account
+{
+    internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+    {
+        public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+
+            if (user is null)
+            {
+                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+            }
+
+            return user;
+        }
+    }
+}
